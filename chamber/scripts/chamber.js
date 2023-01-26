1989a2881421927d97c9774741d5f4c42ef0097e
Naomi Smith <82910046+super-train@users.noreply.github.com>
2023-01-25 20:10:05 -0700

    updates

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -12,17 +12,21 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
-// last modified //
+
+/* last modified */
 let text = document.lastModified;
 document.getElementById("time").innerHTML = text;
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
 
+
 // nav //
  
-const navigation = document.querySelector('.navigation')
-const menu= document.querySelector('#menu');
-
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// copyright//
+document.getElementById("year").innerHTML = new Date().getFullYear();
