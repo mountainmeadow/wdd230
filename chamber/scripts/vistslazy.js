ccc61d6136cc98754c43245e5f0a5dcc58030100
Naomi Smith <82910046+super-train@users.noreply.github.com>
2023-02-21 18:00:52 -0700

    updates

--- chamber/scripts/vistslazy.js
+++ chamber/scripts/vistslazy.js
@@ -20,3 +20,39 @@ localStorage.setItem("visits-ls", numVisits);
 // show todays date demonstration
 todayDisplay.textContent = Date.now();
 //84600000 equals the number of miliseconds in one day.
+
+//lazy loaning//
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px",
+};
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver(items => {
+        items.forEach(item => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach(img => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
+
+
