0365ce67a3af8c0bd23c81a3cb37960ca75468b0
Naomi Smith <82910046+super-train@users.noreply.github.com>
2023-03-03 14:32:22 -0700

    updates

--- chamber/scripts/lazy.js
+++ chamber/scripts/lazy.js
@@ -1,36 +1,35 @@
+// Select the elements in the DOM
+let imagesToLoad = document.querySelectorAll("img[data-src]");
 
-//lazy loaning//
-const imagesToLoad = document.querySelectorAll('img[data-src]');
-
+// Change the placeholder img for the data-src (original img)
 const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
+    image.setAttribute("src", image.getAttribute("data-src"));
     image.onload = () => {
-        image.removeAttribute('data-src');
+        image.removeAttribute("data-src");
     };
 };
 
 const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px 50px 0px",
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
 };
 
-if ('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver(items => {
-        items.forEach(item => {
+// Check if the content is in the viewport and execute the actions
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
             if (item.isIntersecting) {
                 loadImages(item.target);
-                imgObserver.unobserve(item.target);
+                observer.unobserve(item.target);
             }
         });
-    }, imgOptions);
-
-    imagesToLoad.forEach(img => {
-        imgObserver.observe(img);
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
     });
 } else {
     imagesToLoad.forEach((img) => {
         loadImages(img);
     });
 }
-
 
