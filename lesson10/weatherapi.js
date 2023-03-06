c4424dd618ee56107fa3c6aaf5fef70befef15f2
Naomi Smith <82910046+super-train@users.noreply.github.com>
2023-03-06 09:58:45 -0700

    updates to api

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -1,4 +1,24 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat= 37.095169,&lon=-113.575974&appid=a27612c765e3ec0f741de1b08b5f3317';
 // select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+
+
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        // displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
