1318c5178a24c51fb9743f0cb02fcffc8062f654
Naomi Smith <82910046+super-train@users.noreply.github.com>
2023-03-07 13:41:05 -0700

    updates

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,12 +1,48 @@
-const s = 1;
-document.getElementById('wind_speed').innerText = s
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Saint George&appid=a27612c765e3ec0f741de1b08b5f3317&units=imperial';
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const windChill = document.getElementById('wind-chill')
 
-const t = 32;
-document.getElementById('temp').innerText = t
-if (t <= 50 && s > 3.0) {
-    const windchill = 35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + (0.4275 * t * (s ** .016));
-    document.getElementById("wind_chill").innerText = windchill.toFixed(2)
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      // console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
 }
-else {
-    document.getElementById("wind_chill").innerText = "N/A"
+
+apiFetch();
+
+function  displayResults(weatherData) {
+  const temp = weatherData.main.temp
+  const speed = weatherData.wind.speed
+
+  if (temp <= 50 && speed > 3)
+  {
+    windChill.textContent = `${parseInt(getWindChill(temp, speed))}°F`
+  }
+  else{
+    windChill.textContent="N/A"
+  }
+
+  currentTemp.innerHTML = `<strong>${temp.toFixed(0)}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+}
+
+function getWindChill(temp, speed){
+  return 35.74 + (.6215*temp)-(35.75*(speed**.16))+(.4275*temp*(speed**.16))
 }
