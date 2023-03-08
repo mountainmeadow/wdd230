4ed1d2b0a873202856b9861b64c9ccbba51355f4
Naomi Smith <82910046+super-train@users.noreply.github.com>
2023-03-08 14:45:52 -0700

    updates

---
+++ Bountiful Foods/scripts/bountiful.js
@@ -0,0 +1,6 @@
+//nav//
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
